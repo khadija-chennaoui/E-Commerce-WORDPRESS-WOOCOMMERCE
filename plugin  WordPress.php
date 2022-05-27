0e6dbabd65144666bdedb3c13908da530ea0feeb
khadija-chennaoui <khadijach896@gmail.com>
2022-05-27 16:23:25 +0100

    wp-includes

---
+++ plugin  WordPress.php
@@ -0,0 +1,8 @@
+<?php 
+/**
+ * Plugin Name: YOUR PLUGIN BY KHADIJA
+ * Description: Handle the basics with this plugin BY KHADIJA.
+
+ */
+// Remove the admin bar from the front end
+add_filter( 'show_admin_bar', '__return_false' );
